fe3402932728b9174fb5908df61cd77a93623b99
blyzniuk <blyzniuk.oleksandr@gmail.com>
2021-07-01 00:06:43 +0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+      },
+    },
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "UA-201066704-1",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
