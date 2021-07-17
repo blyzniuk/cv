07057d91af8f4a77331847e794e6888cd9604826
blyzniuk <blyzniuk.oleksandr@gmail.com>
2021-07-17 18:52:10 +0300

    Add styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,7 @@ module.exports = {
       },
     },
     "gatsby-plugin-react-helmet",
+    "gatsby-plugin-styled-components",
     "gatsby-plugin-sitemap",
     {
       resolve: "gatsby-plugin-manifest",
